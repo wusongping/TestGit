dfe04061fc9d2a72d595080ff048a9d3addbabf7
wusongping <wusongping301@sina.com>
2019-05-18 10:42:13 +0800

    first commit

---
+++ src/HelloWord.java
@@ -0,0 +1,12 @@
+/**
+ * Author: 吴宋平
+ * Date: 2019-05-18 10:37
+ * Description:
+ */
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord.main");
+    }
+
+}
